3a10efc3ec0c128bc92cd8fbb28b3ac591ef0869
developer <mr.sodiqmirzosattorov17@gmail.com>
2022-01-04 23:34:58 +0500

    first commit

---
+++ resources/vite-apollo/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+	<button @click="handleClick">Add new</button>
+	<pre>{{ users.data }}</pre>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+	data: () => ({
+		users: [],
+		user: null,
+	}),
+	apollo: {
+		users: {
+			query: gql`
+				{
+					users {
+						data {
+							name
+							email
+						}
+					}
+				}
+			`,
+		},
+	},
+	methods: {
+		handleClick() {
+			this.$apollo.mutate({
+				// Query
+				mutation: gql`
+					mutation {
+						createUser(
+							name: "Sodikmirzo Sattorov"
+							email: "mr.sodiqmirzosattorov17@gmail.com"
+							password: "password"
+						) {
+							name
+						}
+					}
+				`,
+			});
+		},
+	},
+};
+</script>
---
+++ resources/vite-apollo/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import * as apolloProvider from './apollo.provider'
+ 
+const app = createApp(App)
+ 
+app.use(apolloProvider.provider);
+app.mount('#app');
